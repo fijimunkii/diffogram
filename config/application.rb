4d9e43d9a587595aa497124a67a2b28bed9f5c2f
Harrison Powers <fijimunkii@gmail.com>
2013-12-03 11:27:37 -0500

    plz work heroku

--- config/application.rb
+++ config/application.rb
@@ -63,5 +63,8 @@ module Diffogram
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # fix4heroku
+    config.assets.initialize_on_precompile = false
   end
 end
