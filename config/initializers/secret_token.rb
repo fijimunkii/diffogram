6905ab63eff7986669a2e7956a7f873cc90f1273
Harrison Powers <fijimunkii@gmail.com>
2013-12-02 20:22:28 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diffogram::Application.config.secret_token = '6e4904887d9d0ed8e28e1590a96fe80b418e3dee794d68de7099ed1f82ab5c907c8d4f52386d0c550ec94a334e2b30c3606efb421415740bb3db80537a1bf959'
